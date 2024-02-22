fd54ab622295a0d8801adcb5ce442f3a373aa5e7
Evan Tardiff <138072760+mt-evan@users.noreply.github.com>
2024-02-22 02:09:05 -0800

    Update open-ai.js
    removed the old code that was commented out

--- config/open-ai.js
+++ config/open-ai.js
@@ -1,15 +1,3 @@
-/*import { Configuration, OpenAIApi } from 'openai';
-import dotenv from 'dotenv';
-dotenv.config();
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
-
-export default openai;*/
-
 import OpenAI from 'openai';
 import dotenv from 'dotenv';
 dotenv.config();
